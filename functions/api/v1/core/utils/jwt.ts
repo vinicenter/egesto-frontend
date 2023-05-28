4bc952f83f8ec826a7474969ca8ccaf1560a109c
vinicenter <viniemidiobosi@gmail.com>
2023-05-27 22:12:20 -0300

    bugfix(core): debug jwt token

--- functions/api/v1/core/utils/jwt.ts
+++ functions/api/v1/core/utils/jwt.ts
@@ -6,6 +6,7 @@ const config = {
 }
 
 const sign = async (payload: JwtPayload, secret: string | JsonWebKey) => {
+  console.log('start sign', payload, secret)
   const token = await jwt.sign({ ...payload, ...config }, secret)
   console.log('sign', token)
   return token
