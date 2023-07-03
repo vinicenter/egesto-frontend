c1c9f3295c8a18e230da9b36780cb9a6b39131dc
vinicenter <viniemidiobosi@gmail.com>
2023-07-02 21:32:03 -0300

    chore(core): remove not necessary serverless libs

--- functions/api/v1/auth/models/user.types.ts
+++
@@ -1,6 +0,0 @@
-export interface IUser {
-  username: string,
-  password: string,
-  name: string,
-  email?: string | null,
-}
--- functions/api/v1/core/utils/response-messages.ts
+++
@@ -1,7 +0,0 @@
-export const successResponse = (data: any, status: number) => {
-  return new Response(JSON.stringify({ ...data }), { status })
-}
-
-export const errorResponse = (error: string, status: number) => {
-  return new Response(JSON.stringify({ error }), { status })
-}
