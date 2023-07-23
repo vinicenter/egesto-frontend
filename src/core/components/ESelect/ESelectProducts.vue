ad549fe9d6216567a593e8801d279f72418479fa
vinicenter <viniemidiobosi@gmail.com>
2023-07-23 15:38:58 -0300

    feat(product): implements produt select

---
+++ src/core/components/ESelect/ESelectProducts.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { getProducts } from '@/src/modules/records/products/datasource/products'
+</script>
+
+<template>
+  <ESelectQuery
+    item-title="name"
+    item-value="_id"
+    label="Produto"
+    queryKey="select-products"
+    :query-fn="getProducts"
+  />
+</template>
--- src/modules/records/families/components/FamiliesForm.vue
+++ src/modules/records/families/components/FamiliesForm.vue
@@ -66,7 +66,10 @@ const loadCostsFromFamily = async (family: IFamily) => {
       </div>
 
       <template v-else>
-        <div v-for="(cost, index) in costs" :key="`cost-${index}`" class="grid grid-cols-1 md:grid-cols-2 gap-x-sm">
+        <div
+          v-for="(cost, index) in costs" :key="`cost-${index}`"
+          class="grid grid-cols-1 md:grid-cols-2 gap-x-sm"
+        >
           <VTextField
             v-model="cost.name"
             :disabled="disabled"
