9cd46a4085b42ed25590704bf7f167bb469281a7
vinicenter <viniemidiobosi@gmail.com>
2023-07-30 01:20:08 -0300

    feat(costsTable): implements clone feature

---
+++ src/modules/costsTable/pages/costsTable/[id]/clone.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import type { ICostsTable } from '../../../types/costsTable';
+
+const emit = defineEmits(['submit']);
+
+defineProps<{
+  model: ICostsTable.Root
+  loading: boolean
+}>()
+</script>
+
+<template>
+  <CostsTableForm
+    :disabled="false"
+    button-label="Clonar"
+    :model="model"
+    :loading="loading"
+    @submit="emit('submit', 'create')"
+  />
+</template>
+
+<route lang="yaml">
+name: clone-costs-table
+path: clonar
+meta:
+  title: Clonar Tabela de Custos
+</route>
