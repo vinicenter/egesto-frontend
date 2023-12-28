e70dbe0b4c8e09ca4ef171ea52ea158be4ca8378
vinicenter <viniemidiobosi@gmail.com>
2023-12-28 00:47:40 -0300

    feat: better initials for user

--- src/core/components/ELayout.vue
+++ src/core/components/ELayout.vue
@@ -101,6 +101,10 @@ const userInitials = computed(() => {
 
   const names = authStorage.value.name.split(' ')
 
+  if(names.length === 1) {
+    return names[0].charAt(0)
+  }
+
   const initials = [
     names[0].charAt(0),
     names[names.length - 1].charAt(0),
