f3cd8423f743905f7df21ea3343b60880b05b386
vinicenter <viniemidiobosi@gmail.com>
2024-03-29 17:39:08 -0300

    refactor(core): remove api global prefix

--- src/core/utils/axios.ts
+++ src/core/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL || ''}/api/v1`,
+  baseURL: `${import.meta.env.VITE_API_URL || ''}/v1`,
 })
 
 export default instance
